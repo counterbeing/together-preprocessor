610c0f378bd07e086fdb3d6845786511695e61cc
Cory Logan <he@corylogan.com>
2019-09-27 14:25:26 -0700

    testing different exif data for use with video

---
+++ src/process-videos.js
@@ -0,0 +1,30 @@
+import md5 from "md5"
+import fs from "fs-extra"
+import { basename } from "path"
+import exiftool from "node-exiftool"
+import exiftoolBin from "dist-exiftool"
+const ep = new exiftool.ExiftoolProcess(exiftoolBin)
+
+async function getData() {
+  await ep.open()
+  const metadata = await ep.readMetadata("./photos/IMG_0595.m4v")
+  ep.close()
+  return metadata
+}
+
+async function run(filename, buffer = null) {
+  if (!buffer) {
+    buffer = await fs.readFile(filename)
+  }
+  let checksum = md5(buffer)
+  let data = await getData()
+  data = data["data"][0]
+  const out = {
+    file: basename(filename),
+    date: data.ContentCreateDate,
+    description: data.Description,
+    checksum
+  }
+  console.log(out)
+}
+run("./photos/IMG_0595.m4v")
