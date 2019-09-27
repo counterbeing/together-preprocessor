82cdfe6136674ca1af510daf8eb51e2911f0c398
Cory Logan <he@corylogan.com>
2019-09-27 00:10:53 -0700

    tidy

--- src/geocoder.js
+++ src/geocoder.js
@@ -1,17 +1,17 @@
-const apiKey = process.env.GEOCODIO_KEY;
-const fetch = require("node-fetch");
+const apiKey = process.env.GEOCODIO_KEY
+const fetch = require("node-fetch")
 
-const geocodioRequest = async (address) => {
+const geocodioRequest = async address => {
   const url = [
-    'https://api.geocod.io/v1.3/geocode?q=',
+    "https://api.geocod.io/v1.4/geocode?q=",
     encodeURIComponent(address),
-    '&api_key=',
-    apiKey,
-  ].join('');
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-};
+    "&api_key=",
+    apiKey
+  ].join("")
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}
 
 // const geocoder = async (city, state) => {
 //   const key = `geocode_${city}_${state}`;
--- stories/2019-07-20-roving-the-craters.md
+++ stories/2019-07-20-roving-the-craters.md
@@ -1,8 +1,8 @@
 ---
-title: 'Roving the Craters'
-location: 'Craters of the Moon, ID'
+title: "Roving the Craters"
+location: "Arco, ID"
 start_date: 2019-07-20T19:00:00.000Z
 end_date: 2019-07-21T23:00:00.000Z
-
 ---
+
 My story goes here!
